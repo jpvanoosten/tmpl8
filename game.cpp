2a57acb2b6550d205b9d8c00fcb06e06ffed12dc
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2021-12-21 23:12:15 +0100

    Part 7 - Debugging.

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include "game.h"
 #include "surface.h"
 #include <cstdio> //printf
+#include <cassert>
 
 namespace Tmpl8
 {
@@ -24,17 +25,27 @@ namespace Tmpl8
 	// -----------------------------------------------------------
 	// Main application tick function
 	// -----------------------------------------------------------
-	void Game::Tick(float deltaTime)
-	{
-		// clear the graphics window
-		screen->Clear(0);
-		// print something in the graphics window
-		screen->Print("hello world", 2, 2, 0xffffff);
-		// print something to the text window
-		printf("this goes to the console window.\n");
-		// draw a sprite
-		rotatingGun.SetFrame(frame);
-		rotatingGun.Draw(screen, 100, 100);
-		if (++frame == 36) frame = 0;
-	}
+    Surface image("assets/ball.png");
+    // -----------------------------------------------------------
+    // Main application tick function
+    // -----------------------------------------------------------
+    void Game::Tick(float deltaTime)
+    {
+        // clear the graphics window
+        screen->Clear(0);
+        // draw a grid
+        for (int x = 15; x < 800; x += 16)
+        {
+            for (int y = 6; y < 480; y += 12)
+            {
+                Pixel p = image.GetBuffer()[x / 16 + (y / 12) * 50];
+                int red = p & 0xff0000;
+                int green = p & 0x00ff00;
+                int blue = p & 0x0000ff;
+                screen->Bar(x, y, x + 12, y + 2, red);
+                screen->Bar(x, y + 4, x + 12, y + 6, green);
+                screen->Bar(x, y + 8, x + 12, y + 10, blue);
+            }
+        }
+    }
 };
