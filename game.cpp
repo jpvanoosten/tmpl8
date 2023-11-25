16ac4c3c824371777ccd47cb43ee4d2f292c7ff1
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-11-25 20:02:38 +0100

    Finish assignment.

--- game.cpp
+++ game.cpp
@@ -2,6 +2,7 @@
 #include "surface.h"
 #include <cstdio> //printf
 #include <cassert>
+#include <algorithm>
 
 namespace Tmpl8
 {
@@ -33,18 +34,30 @@ namespace Tmpl8
     {
         // clear the graphics window
         screen->Clear(0);
-        // draw a grid
-        for (int x = 15; x < 800; x += 16)
+
+        float widthRatio = static_cast<float>(image.GetWidth()) / static_cast<float>(screen->GetWidth());
+        float heightRatio = static_cast<float>(image.GetHeight()) / static_cast<float>(screen->GetHeight());
+        int ledWidth = std::max( static_cast<int>(1.0f / widthRatio), 1);
+        int ledHeight = std::max(static_cast<int>(1.0f / heightRatio / 3.0f), 1);
+
+        Pixel* src = image.GetBuffer();
+        Pixel* dst = screen->GetBuffer();
+        int sw = screen->GetWidth();
+        int iw = image.GetWidth();
+
+        for (int y = 0; y < screen->GetHeight(); y += ledHeight * 3 )
         {
-            for (int y = 6; y < 480; y += 12)
+            int ty = static_cast<int>(y * heightRatio);
+            for (int x = 0; x < screen->GetWidth(); x += ledWidth)
             {
-                Pixel p = image.GetBuffer()[x / 16 + (y / 12) * 50];
+                int tx = static_cast<int>(x * widthRatio);
+                Pixel p = src[ty * iw + tx];
                 int red = p & 0xff0000;
                 int green = p & 0x00ff00;
                 int blue = p & 0x0000ff;
-                screen->Bar(x, y, x + 12, y + 2, red);
-                screen->Bar(x, y + 4, x + 12, y + 6, green);
-                screen->Bar(x, y + 8, x + 12, y + 10, blue);
+                screen->Bar(x, y, x + ledWidth, y + ledHeight, red);
+                screen->Bar(x, y + ledHeight, x + ledWidth, y + ledHeight * 2, green);
+                screen->Bar(x, y + ledHeight * 2, x + ledWidth, y + ledHeight * 3, blue);
             }
         }
     }
