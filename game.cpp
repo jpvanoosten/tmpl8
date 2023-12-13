54d338c503c6ecbaf28bc6388672a1a3a5d8259d
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-12-13 21:43:07 +0100

    Assignment 2-1.

--- game.cpp
+++ game.cpp
@@ -1,9 +1,31 @@
 #include "game.h"
 #include "surface.h"
 #include <cstdio> //printf
+#include <iterator>
+
+#include "template.h"
 
 namespace Tmpl8
 {
+	constexpr Pixel RED = 0xff0000;
+	constexpr Pixel GREEN = 0x00ff00;
+	constexpr Pixel BLUE = 0x0000ff;
+	constexpr Pixel YELLOW = 0xffff00;
+
+	struct Point
+	{
+		int x, y;
+		Pixel color;
+	};
+
+	constexpr Point points[] = {
+		{0, 0, RED },
+		{ScreenWidth-1, ScreenHeight-1, RED},
+		{ScreenWidth-1, 0, RED },
+		{0, ScreenHeight-1, BLUE },
+		{ScreenWidth-1, ScreenHeight-1, BLUE},
+	};
+
 	// -----------------------------------------------------------
 	// Initialize the application
 	// -----------------------------------------------------------
@@ -18,9 +40,6 @@ namespace Tmpl8
 	{
 	}
 
-	static Sprite rotatingGun(new Surface("assets/aagun.tga"), 36);
-	static int frame = 0;
-
 	// -----------------------------------------------------------
 	// Main application tick function
 	// -----------------------------------------------------------
@@ -28,13 +47,13 @@ namespace Tmpl8
 	{
 		// clear the graphics window
 		screen->Clear(0);
-		// print something in the graphics window
-		screen->Print("hello world", 2, 2, 0xffffff);
-		// print something to the text window
-		printf("this goes to the console window.\n");
-		// draw a sprite
-		rotatingGun.SetFrame(frame);
-		rotatingGun.Draw(screen, 100, 100);
-		if (++frame == 36) frame = 0;
+
+		for(int i = 1; i < std::size(points); ++i )
+		{
+			auto p0 = points[i - 1];
+			auto p1 = points[i];
+
+			screen->Line(p0.x, p0.y, p1.x, p1.y, p0.color);
+		}
 	}
 };
