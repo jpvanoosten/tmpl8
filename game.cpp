465e177fbf124477a036b0912d6c4e37650970cb
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-12-13 22:02:34 +0100

    Assignment 3 - 1 & 3 -2.

--- game.cpp
+++ game.cpp
@@ -4,37 +4,52 @@
 
 namespace Tmpl8
 {
-	// -----------------------------------------------------------
-	// Initialize the application
-	// -----------------------------------------------------------
-	void Game::Init()
-	{
-	}
-	
-	// -----------------------------------------------------------
-	// Close down application
-	// -----------------------------------------------------------
-	void Game::Shutdown()
-	{
-	}
+    // -----------------------------------------------------------
+    // Initialize the application
+    // -----------------------------------------------------------
+    void Game::Init()
+    {
+    }
 
-	static Sprite rotatingGun(new Surface("assets/aagun.tga"), 36);
-	static int frame = 0;
+    // -----------------------------------------------------------
+    // Close down application
+    // -----------------------------------------------------------
+    void Game::Shutdown()
+    {
+    }
 
-	// -----------------------------------------------------------
-	// Main application tick function
-	// -----------------------------------------------------------
-	void Game::Tick(float deltaTime)
-	{
-		// clear the graphics window
-		screen->Clear(0);
-		// print something in the graphics window
-		screen->Print("hello world", 2, 2, 0xffffff);
-		// print something to the text window
-		printf("this goes to the console window.\n");
-		// draw a sprite
-		rotatingGun.SetFrame(frame);
-		rotatingGun.Draw(screen, 100, 100);
-		if (++frame == 36) frame = 0;
-	}
+    void Game::DrawI(int x, int y)
+    {
+        screen->Line(100 + x, 50 + y, 200 + x, 50 + y, 0xffffff);
+        screen->Line(150 + x, 50 + y, 150 + x, 300 + y, 0xffffff);
+        screen->Line(100 + x, 300 + y, 200 + x, 300 + y, 0xffffff);
+    }
+
+    void Game::DrawFatI(int x, int y, int thickness)
+    {
+        for(int i = 0; i <= thickness; ++i)
+        {
+            for(int j = 0; j <= thickness; ++j)
+            {
+                DrawI(x + i, y + j);
+            }
+        }
+    }
+
+    // -----------------------------------------------------------
+    // Main application tick function
+    // -----------------------------------------------------------
+    void Game::Tick(float deltaTime)
+    {
+        deltaTime /= 1000.0f;
+
+        // clear the graphics window
+        screen->Clear(0);
+
+        const float speed = 10.0f;
+
+        DrawFatI(static_cast<int>(x), static_cast<int>(y), 1);
+
+        x += deltaTime * speed;
+    }
 };
--- game.h
+++ game.h
@@ -12,11 +12,16 @@ public:
 	void Tick( float deltaTime );
 	void MouseUp( int button ) { /* implement if you want to detect mouse button presses */ }
 	void MouseDown( int button ) { /* implement if you want to detect mouse button presses */ }
-	void MouseMove( int x, int y ) { /* implement if you want to detect mouse movement */ }
+	void MouseMove( int _x, int _y ) { /* implement if you want to detect mouse movement */ }
 	void KeyUp( int key ) { /* implement if you want to handle keys */ }
 	void KeyDown( int key ) { /* implement if you want to handle keys */ }
+	void DrawI(int x, int y);
+	void DrawFatI(int x, int y, int thickness = 1);
 private:
 	Surface* screen;
+
+	float x = 100.0f;
+	float y = 100.0f;
 };
 
 }; // namespace Tmpl8
